12198323e3dc1d37394e072e9ff81239bb71e632
Etson <etsond@ufl.edu>
2024-04-23 09:56:49 -0400

    networth

--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -8,3 +8,15 @@ var debt = 200;
 var networth = assets - debt;
 
 console.log(`Your net worth is ${networth}`);
+
+
+
+// or
+
+function calculateNetWorth(assets, debt){
+    return assets - debt;
+}
+
+var networth = calculateNetWorth(1000000, 200);
+
+console.log(`Your networth is ${networth}`);
