e3c10673c96e5045a2c9639c8f787d1f17b7a1b2
Etson <etsond@ufl.edu>
2024-04-24 14:19:42 -0400

    adding without func

--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -14,3 +14,17 @@ function calculateGrossPay(payRate, hoursWorked){
 calculateGrossPay(12.5, 20);
 calculateGrossPay(25,40);
 calculateGrossPay(17.30,45);
+
+
+
+var payRate = 12.5
+var hoursWorked = 20
+
+ if(hoursWorked <= 40){
+        console.log((payRate * hoursWorked).toFixed(2));
+    }
+    else{
+        var regularPay = payRate * 40;
+        var overtimPay = (hoursWorked - 40) * (payRate * 1.5)
+        console.log((regularPay + overtimPay).toFixed(2));
+    }
