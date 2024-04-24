799ae206a820f1e418deb2f8d7755f5b40645c3b
Etson <etsond@ufl.edu>
2024-04-24 11:05:00 -0400

    numeric conversions

--- MathScripts/taxes.js
+++ MathScripts/taxes.js
@@ -1,7 +1,9 @@
 "use scripts"
 
+// set amount of taxes
 var salary = 5000;
 
+// tax that is 
 var taxWithHeld = (23/100) * salary;
 
 console.log("Your federal taxes withheld "+ taxWithHeld.toFixed(2));
---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// description: This script tests various numeric
+// conversion techniques
+// author Jordan Q. Newprogrammer;
+
+
+// defie the following variables in your script:
+
+var a = "  101.1  ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5   ";
+
+// perform following tasks:
+
+// 1 convert each using parseInt() and display results;
+console.log("paseInt() results")
+
+var aInt = parseInt(a);
+console.log("a is"+ aInt);
+// b
+var bInt = parseInt(b);
+console.log("b is " + bInt);
+// c
+var cInt = parseInt(a);
+console.log("c is " + cInt);
+// d
+var dInt = parseInt(d);
+console.log("d is " + dInt);
+
+console.log("parseFloat result")
+
+// parsefloat
+var aFloat = parseFloat(a);
+console.log("a is"+ aFloat);
+
+// b
+var bFloat = parseFloat(b);
+console.log("b is " + bFloat);
+// c
+var cFloat = parseFloat(a);
+console.log("c is " + cFloat);
+// d
+var dFloat = parseFloat(d);
+console.log("d is " + dFloat);
+
+console.log("THis ")
+// Number
+console.log("Number section");
+var aNum = Number(a);
+console.log("a is"+ aNum);
+
+// b
+var bNum = Number(b);
+console.log("b is " + bNum);
+// c
+var cNum = Number(a);
+console.log("c is " + cNum);
+// d
+var dNum = Number(d);
+console.log("d is " + dNum);
