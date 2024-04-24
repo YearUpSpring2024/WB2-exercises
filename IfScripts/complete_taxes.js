77c121190f97ef1aa54880c4be88089c1ebf4e4c
Etson <etsond@ufl.edu>
2024-04-24 14:57:04 -0400

    complete taxes

---
+++ IfScripts/complete_taxes.js
@@ -0,0 +1,49 @@
+"use script";
+
+function calculateGrossPay(payRate, hoursWorked){
+    if(hoursWorked <= 40){
+        return((payRate * hoursWorked).toFixed(2));
+    }
+    else{
+        var regularPay = payRate * 40;
+        var overtimPay = (hoursWorked - 40) * (payRate * 1.5)
+        return((regularPay + overtimPay).toFixed(2));
+    }
+}
+
+// calculateGrossPay(12.5, 20);
+// calculateGrossPay(25,40);
+// calculateGrossPay(17.30,45);
+
+var payRate = 10;
+var hoursWorked = 45;
+
+
+var weeklyGrossPay = calculateGrossPay(payRate,hoursWorked);
+
+// get the annual pay
+var anualGrossPay = weeklyGrossPay *52;
+
+// intializing the taxRate
+var taxRate; 
+// now adding tax
+if(anualGrossPay < 12000){
+    taxRate = 0.05;
+}else if(anualGrossPay < 25000){
+    taxRate = 0.10;
+}else if (anualGrossPay < 75000){
+    taxRate = 0.15;
+}else{
+    taxRate = 0.20;
+}
+
+// how much tax they hold
+let taxHold = weeklyGrossPay * taxRate;
+// get the net
+let net = weeklyGrossPay - taxHold;
+
+console.log("You worked " + hoursWorked + " hours this period ");
+console.log("Because you earn $" + payRate + " per hour, your gross pay is $" + weeklyGrossPay);
+console.log("Your filing status is Single");
+console.log("Your tax withholdings this period is $"+ taxHold);
+console.log("Your net pay is $" + net);
