00e5a550d3227cdd4fafe30516229e38ced0d50d
Etson <etsond@ufl.edu>
2024-04-25 13:33:19 -0400

    show major

--- IfScripts/show_major.js
+++ IfScripts/show_major.js
@@ -1,4 +1,32 @@
 "use strict";
 
-let studentMajor = "BIOL";
-let 
+// intialize the student name
+let studentName = "Betty";
+let studentMajor = "ENG";
+
+let majorName;
+let advisorLo;
+
+
+if (studentMajor === "BIOL") {
+    majorName = "Biology";
+    advisorLo = "science bldg, Room 310";
+} else if (studentMajor === "CSCI") {
+    majorName = "Computer science";
+    advisorLo = "Sheppard Hall,Room 314";
+}
+else if (studentMajor === "ENG") {
+    majorName = "English";
+    advisorLo = "Kerr Hall, Room 201";
+}
+else if (studentMajor === "HIST") {
+    majorName = "History";
+    advisorLo = "Kerr Hall, Room 114";
+}
+else if (studentMajor === "MKT") {
+    majorName = "Marketing";
+    advisorLo = "Wesley Hall, Room 310";
+}
+console.log("Student: " + studentName);
+console.log("Major: " + studentMajor);
+console.log("Advising Location: " + advisorLo);
